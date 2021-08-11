f07ab5575253cf4df0d721cc015cb949739b9dda
pedrovitordev <69403840+pedrovitordev@users.noreply.github.com>
2021-08-11 15:15:48 -0300

    Funções em Js

---
+++ funcao/funcaoContrutora.js
@@ -0,0 +1,31 @@
+function Carro(velociadeMaxima= 200, delta= 5) {
+    // atributo privado
+    let velocidadeAtual = 0
+
+    // metodo publico
+    this.acelerar = function () {
+        if (velocidadeAtual + delta <= velociadeMaxima) {
+            velocidadeAtual += delta
+        } else {
+            velocidadeAtual = velociadeMaxima
+        }
+    }
+
+    // metodo publico
+    this.getVelocidadeAtual = function () {
+        return velocidadeAtual
+    }
+}
+
+const uno = new Carro
+uno.acelerar()
+console.log(uno.getVelocidadeAtual())
+
+const ferrari = new Carro(350, 20)
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+console.log(ferrari.getVelocidadeAtual())
+
+console.log(typeof Carro)
+console.log(typeof ferrari)
